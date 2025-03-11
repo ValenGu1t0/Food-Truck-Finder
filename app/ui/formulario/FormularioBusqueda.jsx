f1fd6be91a0fa485e97e1baa3392ceb6e7609d5b
ValenGu1t0 <devalen543@gmail.com>
2025-03-11 12:08:36 -0300

    Update SSR

--- app/ui/formulario/FormularioBusqueda.jsx
+++ app/ui/formulario/FormularioBusqueda.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {useState} from "react";
+import { useState } from "react";
 
 /* Obtenemos la funcion que actualiza el estado desde Home */
 export default function FormularioBusqueda( { handleSubmit } ) {
@@ -25,6 +25,7 @@ export default function FormularioBusqueda( { handleSubmit } ) {
         (
             
             <div className="flex flex-col justify-center items-start p-8 gap-6 bg-purple-600 rounded-xl shadow-2xl transition-all duration-500">
+                
                 <h2 className="font-mono text-2xl font-semibold text-indigo-950">Welcome to Food-Finder!</h2>
                 <p className="font-sans text-md text-slate-100 text-justify">Discover the best food-trucks in <strong className="text-indigo-950">San Francisco</strong>! Type the kind of meal you want and set a distance range.</p>
                 <p className="font-sans text-md text-slate-100 text-justify">If you want to see any type of food near you directly, just hit Search!</p>
@@ -34,6 +35,7 @@ export default function FormularioBusqueda( { handleSubmit } ) {
                 className="font-sans bg-purple-800 text-white font-semibold border-2 cursor-pointer px-4 py-2 rounded-lg hover:bg-purple-950 hover:border-black transition duration-200 ease-in-out">
                     Start Searching
                 </button>
+
             </div>
             
         ) : (
@@ -43,11 +45,14 @@ export default function FormularioBusqueda( { handleSubmit } ) {
                 <h2 className="font-mono text-2xl font-semibold text-indigo-950">Find your perfect Food-Truck!</h2>
 
                 <div className="flex flex-col justify-center items-start gap-1 w-full">
+
                     <label htmlFor="meal" className="text-lg font-sans">
                         What kind of meal would you like?
                     </label>
+
                     <input id="meal" type="text" value={meal} onChange={(e) => setMeal(e.target.value)} placeholder="Only if you're looking for something special." 
                     className="p-2 w-full bg-gray-100 border border-white rounded-lg focus:border-purple-950 focus:outline-none"  />
+                
                 </div>
 
                 <div className="flex flex-col justify-center items-start gap-2 w-full">
--- app/ui/mapa/Mapa.jsx
+++ app/ui/mapa/Mapa.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import L from "leaflet";
+import { useState, useEffect } from "react";
 
 // Cargar react-leaflet dinámicamente y deshabilitar SSR para usar Mapa en el client
 import dynamic from "next/dynamic";
@@ -17,19 +17,33 @@ const Marker = dynamic(() => import("react-leaflet").then((mod) => mod.Marker), 
 // Instanciamos popup, elemento que muestra información a partir de un marcador
 const Popup = dynamic(() => import("react-leaflet").then((mod) => mod.Popup), { ssr: false } );
 
-// Icono para marcadores
-const markerIcon = new L.Icon({ iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-iconSize: [20, 30], iconAnchor: [12, 41], popupAnchor: [1, -34] });
-
-// Icono para posicion inicial (User)
-const userMarkerIcon = L.icon({ iconUrl: "/icons/marker.png", 
-iconSize: [32, 32], iconAnchor: [16, 32], popupAnchor: [0, -32] });
-
-
 
 // San Francisco - lat y lng iniciales desde Home // Recibe foodTrucks actuales desde Home 
 export default function Mapa({ lat, lng, zoom = 12, foodTrucks}) {
 
+    
+    // Evitamos que leaflet se cargue en el servidor
+    const [L, setL] = useState(null);
+
+    useEffect(() => {
+
+        import("leaflet").then((leaflet) => {  setL(leaflet);  });
+
+    }, []);
+
+    if (!L) return <p>Cargando mapa...</p>;
+
+
+    // Icono para marcadores
+    const markerIcon = new L.Icon({ iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+    iconSize: [20, 30], iconAnchor: [12, 41], popupAnchor: [1, -34] });
+    
+    // Icono para posicion inicial (User)
+    const userMarkerIcon = L.icon({ iconUrl: "/icons/marker.png", 
+    iconSize: [32, 32], iconAnchor: [16, 32], popupAnchor: [0, -32] });
+
+
+    // Componente Map
     return (
 
         <Map center={[lat, lng]} zoom={zoom} className="w-full h-[500px] rounded-xl shadow-xl">
